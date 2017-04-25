07af1b40b5e7db762dd55eb010b4ee00ce42cf77
Zevran <ziyuangong@gmail.com>
2017-04-25 11:29:42 -0400

    web service

---
+++ application.py
@@ -0,0 +1,23 @@
+#!flask/bin/env python
+from flask import Flask, jsonify, make_response
+
+application = Flask(__name__)
+app = application
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/RESTapi/questions/<string:q>', methods=['GET'])
+def get_tasks(q):
+	result = str(len(q))
+	if len(result)==0:
+		abort(404)
+	return jsonify({'response': result})
+
+@app.errorhandler(404)
+def no_response(error):
+	return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True, host='0.0.0.0')
